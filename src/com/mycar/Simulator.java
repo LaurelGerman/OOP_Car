d002b1738be59289f6c76819c4ff58ca19c35846
Laurel German <laurel.german@gmail.com>
2021-04-03 13:18:54 -0400

    Set up project structure

---
+++ src/com/mycar/Simulator.java
@@ -0,0 +1,15 @@
+package com.mycar;
+
+import com.mycar.models.Car;
+
+public class Simulator {
+	
+	public static void main(String[] args) {
+		
+		Car myCar = new Car();
+		myCar.run();
+		
+		
+	}
+
+}
---
+++ src/com/mycar/models/Car.java
@@ -0,0 +1,9 @@
+package com.mycar.models;
+
+public class Car {
+	
+	public void run(){
+		
+	}
+
+}
